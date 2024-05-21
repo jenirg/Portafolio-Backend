5a03ac49afa27b3a5538e0ca60ba697d0216674f
jenirg <jeniferrabanales99@gmail.com>
2024-05-21 00:44:11 -0600

    agregando cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 
 const express = require('express');
 const db = require('./db');
@@ -7,6 +8,7 @@ const rutasBlogs = require('./blogs');
 const app = express();
 app.use(express.json());
 app.use('/api/blogs', rutasBlogs);
+app.use(cors());
 
 db.connect();
 app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
