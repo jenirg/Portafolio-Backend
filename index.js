af76eded75bb6b7a4c370d9403258c3e2a190da3
jenirg <jeniferrabanales99@gmail.com>
2024-05-21 10:01:34 -0600

    fixing cors configuration

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const db = require('./db');
 const rutasBlogs = require('./blogs');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', rutasBlogs);
-app.use(cors());
-
 db.connect();
 app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
